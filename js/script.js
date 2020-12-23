94ecb3687393a5a34369dca9f56e2b8f41fe41e9
nicholas-oduor <oduor5742@gmail.com>
2020-12-23 10:26:53 +0300

    added functions in js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// what we do section
+$(document).ready(function () {
+    $("#design-pic").click(function () {
+        $("#design-p").show();
+        $("#design-pic").hide();
+    })
+    $("#design-p").click(function () {
+        $("#design-p").hide();
+        $("#design-pic").show();
+    })
+    $("#development-pic").click(function () {
+        $("#development-p").show();
+        $("#development-pic").hide();
+    })
+    $("#development-p").click(function () {
+        $("#development-p").hide();
+        $("#development-pic").show();
+    });
+    $("#product-pic").click(function () {
+        $("#product-p").show();
+        $("#product-pic").hide();
+    })
+    $("#product-p").click(function () {
+        $("#product-p").hide();
+        $("#product-pic").show();
+    });
+});
